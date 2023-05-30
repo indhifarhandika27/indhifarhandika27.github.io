d08ac85549762ec3e11bd06a3d5819f813d7bf14
indhifarhandika27 <116496372+indhifarhandika27@users.noreply.github.com>
2023-05-30 13:32:19 +0700

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,66 @@
+var linkedin = "https://www.linkedin.com/in/indhifarhandika/";
+var instagram = "https://www.instagram.com/indhifarhandika/";
+var github = "https://github.com/indhifarhandika27/";
+var email = 'mailto:indhifarhandika27@icloud.com';
+var password = 'hahaha';
+
+whois = [
+    "<br>",
+    "Hey, I'm Indhi",
+    "I'm a software developer",
+    "<br>"
+];
+
+whoami = [
+    "<br>",
+    "The paradox of Who am I? we never know, but, we constantly find out.",
+    "<br>"
+];
+
+social = [
+    "<br>",
+    'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
+    'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
+    'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+    "<br>"
+];
+
+secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you\'re admin',
+    "<br>"
+];
+
+projects = [
+    "<br>",
+    "Still curating... most projects are offline, on GitHub, or confidential.",
+    "<br>"
+];
+
+help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Forrest?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">social</span>         Display social networks',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">email</span>          Do not email me',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+];
+
+banner = [
+    '<span class="index">IFR Dev Not A Corporation. All knights reserved.</span>',
+    "██▓ ███▄    █ ▓█████▄  ██░ ██  ██▓     █████▒▄▄▄       ██▀███   ██░ ██  ▄▄▄       ███▄    █ ▓█████▄  ██▓ ██ ▄█▀▄▄▄      ",
+    "▓██▒ ██ ▀█   █ ▒██▀ ██▌▓██░ ██▒▓██▒   ▓██   ▒▒████▄    ▓██ ▒ ██▒▓██░ ██▒▒████▄     ██ ▀█   █ ▒██▀ ██▌▓██▒ ██▄█▒▒████▄    ",
+    "▒██▒▓██  ▀█ ██▒░██   █▌▒██▀▀██░▒██▒   ▒████ ░▒██  ▀█▄  ▓██ ░▄█ ▒▒██▀▀██░▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██▒▓███▄░▒██  ▀█▄  ",
+    "░██░▓██▒  ▐▌██▒░▓█▄   ▌░▓█ ░██ ░██░   ░▓█▒  ░░██▄▄▄▄██ ▒██▀▀█▄  ░▓█ ░██ ░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌░██░▓██ █▄░██▄▄▄▄██ ",
+    "░██░▒██░   ▓██░░▒████▓ ░▓█▒░██▓░██░   ░▒█░    ▓█   ▓██▒░██▓ ▒██▒░▓█▒░██▓ ▓█   ▓██▒▒██░   ▓██░░▒████▓ ░██░▒██▒ █▄▓█   ▓██▒",
+    "░▓  ░ ▒░   ▒ ▒  ▒▒▓  ▒  ▒ ░░▒░▒░▓      ▒ ░    ▒▒   ▓▒█░░ ▒▓ ░▒▓░ ▒ ░░▒░▒ ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒ ░▓  ▒ ▒▒ ▓▒▒▒   ▓▒█░",
+    " ▒ ░░ ░░   ░ ▒░ ░ ▒  ▒  ▒ ░▒░ ░ ▒ ░    ░       ▒   ▒▒ ░  ░▒ ░ ▒░ ▒ ░▒░ ░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒  ▒ ░░ ░▒ ▒░ ▒   ▒▒ ░",
+    " ▒ ░   ░   ░ ░  ░ ░  ░  ░  ░░ ░ ▒ ░    ░ ░     ░   ▒     ░░   ░  ░  ░░ ░  ░   ▒      ░   ░ ░  ░ ░  ░  ▒ ░░ ░░ ░  ░   ▒   ",
+    " ░           ░    ░     ░  ░  ░ ░                  ░  ░   ░      ░  ░  ░      ░  ░         ░    ░     ░  ░  ░        ░  ░",
+    "                ░                                                                             ░                          ",
+    '<span class="color2">Welcome to my interactive web terminal.</span>',
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
