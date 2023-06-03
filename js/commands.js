2604aa4db9ed0b9a3de966515ed6b04b624e0802
indhifarhandika27 <116496372+indhifarhandika27@users.noreply.github.com>
2023-06-03 16:13:23 +0700

    update

--- js/commands.js
+++ js/commands.js
@@ -43,7 +43,7 @@ projects = [
 
 help = [
     "<br>",
-    '<span class="command">whois</span>          Who is IFR Dev?',
+    '<span class="command">whois</span>          Who is indhi?',
     '<span class="command">whoami</span>         Who are you?',
     '<span class="command">social</span>         Display social networks',
     '<span class="command">help</span>           You obviously already know what this does',
